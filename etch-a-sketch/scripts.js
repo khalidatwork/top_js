1ea8e3b566ea64c383ea85ce64c99402eefa77a0
Khalid Kassi <khalid.kassi.extern@tagueri.com>
2025-07-25 17:16:56 +0100

    final

--- etch-a-sketch/scripts.js
+++ etch-a-sketch/scripts.js
@@ -1,21 +1,55 @@
 
 const gridContainer = document.querySelector(".gridContainer")
-let cell 
+const gridSize = 400
 
 function pexilate(elem){
-
-    elem.target.style.backgroundColor = "gray"
-    console.log("here")
+    elem.target.style.backgroundColor =  `rgb(${(Math.random())*200}, ${(Math.random())*200}, ${(Math.random())*200})`
+    // elem.target.style.backgroundColor =  `rgb(${(Math.random())*100, (Math.random())*100, (Math.random())*100})`
 }
 
-const N = 100
-
-for(let i = 0; i<N; i++){
-    cell = document.createElement("div")
-    cell.addEventListener("mouseover", pexilate)
-    cell.style.width = `calc(100% / ${N/2})`;
-    cell.style.height = `calc(100% / ${N/2})`;
+function createGrid(N){
     
-    gridContainer.appendChild(cell)
+    const cellWidth = 100 / N; // Calculate percentage width
+    let cell 
+    gridContainer.style.setProperty('--grid-size', N/4);
+    gridContainer.style.setProperty('--cell-size', `${N*3}px`);
+    gridContainer.style.setProperty('width', `${gridSize}px`);
+    gridContainer.style.setProperty('height', `${gridSize}px`);
+
+    for(let i = 0; i<N*N; i++){
+        cell = document.createElement("div")
+
+        cell.className = 'gridCell';
+        cell.style.setProperty('flex',`0 0 ${cellWidth}%`) 
+        cell.style.setProperty('width',`${gridSize/N}px`  ) 
+        cell.style.setProperty('height', `${gridSize/N}px` ) 
+
+        cell.addEventListener("mouseover", pexilate)
+
+        gridContainer.appendChild(cell)
+    }
+
 }
 
+function resetGrid(element){
+    
+    let N = parseInt(prompt("Input the number of squares per side !")) 
+
+    if(N == null){
+        N = 16
+        return ;
+    } 
+
+    while (N > 100) {
+        N = parseInt(prompt("Please input a value less than 100 !")) 
+    }
+        
+    gridContainer.innerHTML = ""
+    createGrid(N)
+    
+}
+
+const resetButton = document.querySelector("#reset")
+resetButton.addEventListener("click", resetGrid)
+
+createGrid(16)
